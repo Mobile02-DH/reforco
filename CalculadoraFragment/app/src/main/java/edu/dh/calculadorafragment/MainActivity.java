405f8a3b577ad4ead55404f61cf1c7532d9ab064
unknown <andres@digitalhouse.com>
2018-08-29 22:53:32 -0300

    exercicio 29_08

--- CalculadoraFragment/app/src/main/java/edu/dh/calculadorafragment/MainActivity.java
+++ CalculadoraFragment/app/src/main/java/edu/dh/calculadorafragment/MainActivity.java
@@ -7,21 +7,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements BlankFragment.OnFragmentInteractionListener {
+	BlankFragment txtFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		txtFragment = new BlankFragment();
+		FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+		fragmentTransaction.add(R.id.flNumero, txtFragment);
+		fragmentTransaction.commit(); // commit/consolidar
+
 	}
 
 	public void inserirNumero(View view) {
-
+		Button botao = (Button) view;
+		txtFragment.setTxtQualquerCoisa(botao.getText().toString());
 	}
 
 	public void inserirOperacao(View view) {
+
+	}
+
+	@Override
+	public void onFragmentInteraction(Uri uri) {
 
 	}
 }
